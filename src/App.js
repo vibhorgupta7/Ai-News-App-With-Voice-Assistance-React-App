81350867431827cb07ba896b040e762d9de573ec
vibhorgupta7 <vibhor4k@gmail.com>
2021-03-07 00:38:26 +0530

    NewsCards & Newscard structure added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React, { useEffect }from 'react';
+import React, { useState, useEffect }from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
+import NewsCards from './components/NewsCards/NewsCards'
 
 const alanKey='02c73ee0ad51271f7d61f45d31e434aa2e956eca572e1d8b807a3e2338fdd0dc/stage';
 
-const App = () => {
+
+const App = () => { 
+    const[newsArticles,setNewsArticles] = useState([]);
 
     useEffect(()=>{
         alanBtn({
             key: alanKey,
             onCommand: ({ command, articles}) => {
                     if(command === 'newHeadlines'){
-                        console.log(articles);
+                        setNewsArticles(articles);
                     }
             }
         });
-    }, [])
+    }, []);
+
+
     return(
         <div>
-            <h1>Alan Ai News App</h1>
+            <h1>Alan AI News App</h1>
+            <NewsCards articles={newsArticles}/>
         </div>
     )
+
 }
+
 
 export default App;
---
+++ src/components/NewsCards/NewsCards.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import NewsCard from '../NewsCard/NewsCard';
+
+
+const NewsCards = ({articles}) => {
+    
+    return(
+        <div>
+            { articles.map((article, i)=> (
+                    <NewsCard />
+                ))
+            }
+        </div>
+    ) 
+}
+
+export default NewsCards;
