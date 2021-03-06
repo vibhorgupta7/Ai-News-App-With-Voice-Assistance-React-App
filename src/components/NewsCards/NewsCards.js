bdede1e05ce7b31278461fdc6ad18b0a6c62049d
vibhorgupta7 <vibhor4k@gmail.com>
2021-03-07 02:21:12 +0530

    Newscards & Newscard Completed

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Grid, Grow, Typography } from '@material-ui/core';
 import NewsCard from '../NewsCard/NewsCard';
+import useStyles from './styles.js';
 
 
-const NewsCards = ({articles}) => {
-    
-    return(
-        <div>
-            { articles.map((article, i)=> (
-                    <NewsCard />
+export const NewsCards = ({articles}) => {
+    const classes = useStyles();
+
+    return (
+        <Grow in>
+            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+            {articles.map((article, index)=> (
+                    <Grid item xs={12} sm={8} md={4} lg={3} style={{display:'flex'}}>
+                        <NewsCard article={article} i={index}/>
+                    </Grid>
+                    
                 ))
             }
-        </div>
+            </Grid>
+            
+        </Grow>
     ) 
 }
 
