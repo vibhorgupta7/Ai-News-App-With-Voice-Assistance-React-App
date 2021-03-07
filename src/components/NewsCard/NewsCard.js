5f0cd2692f3b4aaf226bd5f00c251257c6ad9c52
vibhorgupta7 <vibhor4k@gmail.com>
2021-03-07 15:49:08 +0530

    Automatic scroll feature complete

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect, createRef} from 'react';
 import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
 import { Grid } from '@material-ui/core';
 import useStyles from './styles';
 
+
+
 export const NewsCard = ({article:{ description, publishedAt, source, title, url, urlToImage }, i,activeArticle }) => {
     const classes = useStyles();
+    const [elRefs,setElRefs] = useState([]);        // element ref for scrolling automatically
+    const scrollToRef = (ref) => window.scroll(0, ref.current.offsetTop -50);   // this will get the active article's ref and will scrol over top of that card
 
+    useEffect(()=> {
+      setElRefs((refs)=> Array(20).fill().map((_,j) => refs[j] || createRef()));        // creating refs for all cards when page is loaded, 20 because there are 20 articles
+    },[]);
+
+    useEffect(()=> {
+        if( i === activeArticle && elRefs[activeArticle])     // if i=== active artciles and we have ref to active article
+        {
+          scrollToRef(elRefs[activeArticle]);
+        }
     
-
+      },[i, activeArticle,elRefs]);
     
     return (
-        <Card  className={ activeArticle === i ? classes.activeCard : classes.card}>
+        <Card ref={elRefs[i]}  className={ activeArticle === i ? classes.activeCard : classes.card}>
           <CardActionArea href={url} target="_blank">       
             <CardMedia className={classes.media} image={urlToImage || 'https://www.industry.gov.au/sites/default/files/August%202018/image/news-placeholder-738.png'} title={title} />
             <div className={classes.details}>
